5dc355bc2000b59c5a6a8b3752f9bd3573a086a5
PhucVM2019 <phucvm.it.hotmail@gmail.com>
2023-08-25 15:07:34 +0700

    Connection database, check connection database

---
+++ src/main/java/com/infor/vmp/config/DatabaseConnectionTest.java
@@ -0,0 +1,34 @@
+package com.infor.vmp.config;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseConnectionTest implements InitializingBean {
+	private static final Logger log = LogManager.getLogger(DatabaseConnectionTest.class);
+	private DataSource dataSource = null;
+
+	@Autowired
+	public DatabaseConnectionTest(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception  {
+		log.info("BEGIN CHECK CONNECT TO DATABASE");
+		try (Connection connection = dataSource.getConnection()) {
+			log.info("Connected to the database successfully!");
+		} catch (SQLException e) {
+			log.error("Failed to connect to the database: " + e.getMessage());
+		}
+	}
+
+}
